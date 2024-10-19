03cd3a362e27fed36ab0753226bc9396239c7f25
taxin39 <162016838+taxin39@users.noreply.github.com>
2024-10-19 12:16:03 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+// 対象の要素を取得
+const elements1 = document.querySelectorAll('.fade-in');
+
+// オプション設定
+const options = {
+    root: null, // ビューポートを基準に
+    threshold: 0.1 // 要素の10%が表示されたときにコールバック
+};
+
+// コールバック関数
+const callback1 = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible'); // visibleクラスを追加
+        } else {
+            entry.target.classList.remove('visible'); // visibleクラスを削除
+        }
+    });
+};
+
+// インターセクションオブザーバーのインスタンスを作成
+const observer1 = new IntersectionObserver(callback1, options);
+
+// 各要素を監視
+elements1.forEach(element => {
+    observer1.observe(element);
+});
+
+
+// 対象の要素を取得
+const elements2 = document.querySelectorAll('.fade-left');
+
+// コールバック関数
+const callback2 = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible'); // visibleクラスを追加
+        } else {
+            entry.target.classList.remove('visible'); // visibleクラスを削除
+        }
+    });
+};
+
+// インターセクションオブザーバーのインスタンスを作成
+const observer2 = new IntersectionObserver(callback2, options);
+
+// 各要素を監視
+elements2.forEach(element => {
+    observer2.observe(element);
+});
